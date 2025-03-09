53a08d5d2cfdc2e2addd2a09f699f0d66b90ddcf
shivani1720-tech <shivani.sharma17032000@gmail.com>
2025-03-09 01:05:33 -0500

    Update Data_Storage_Postgre.sql

--- Data_Storage_Postgre.sql
+++ Data_Storage_Postgre.sql
@@ -1,1 +1,20 @@
+CREATE DATABASE air_quality_db;
 
+CREATE TABLE air_quality (
+    Year INT,
+    Month INT,
+    Day INT,
+    Hour TEXT,
+    Station TEXT,
+    O3_ppb FLOAT,
+    PM2_5 FLOAT,
+    NO2_ppb FLOAT,
+    SO2_ppb FLOAT,
+    CO_ppm FLOAT
+);
+
+#The below prompt execute in pg cmd 
+COPY air_quality
+FROM 'C:\Users\getsh\OneDrive\Desktop\PBL\Data\Air_Quality_Data\merged_air_quality_data_2012_2025.csv'
+DELIMITER ','
+CSV HEADER;
